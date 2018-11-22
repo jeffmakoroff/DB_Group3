a41a094f30b7ce886f7d1543f439feb4845c571e
Jmako <jmakoroff@gmail.com>
2018-11-21 19:14:51 -0600

    Initial commit
    Here is the start for the program.
    
    Main contains the java connection.
    CSVLoader is for loading a CSV file into the DB.

---
+++ commons-lang3/src/Main.java
@@ -0,0 +1,37 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main {
+
+    private static String JDBC_CONNECTION_URL =
+            "jdbc:oracle:thin:SCOTT/TIGER@localhost:1500:MyDB";
+
+
+    public static void main(String[] args) {
+        try {
+
+            CSVLoader loader = new CSVLoader(getCon());
+
+            loader.loadCSV("C:\\employee.sql", "CUSTOMER", true);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static Connection getCon() {
+        Connection connection = null;
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            connection = DriverManager.getConnection(JDBC_CONNECTION_URL);
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return connection;
+    }
+}
